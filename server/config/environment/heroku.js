8d270a8471224c876d3aa23c3a17ae723b4809ac
Arun Kumar <arun.kumar@tothenew.com>
2016-03-03 16:26:49 +0530

    Added heroku env file

---
+++ server/config/environment/heroku.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: process.env.MONGOURL || 'mongodb://localhost/prwithyoman-dev'
+  },
+  seedDB: false
+};

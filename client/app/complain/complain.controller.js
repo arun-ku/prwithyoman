763a10711d36dcbdff33c041c22b352b71a15322
Arun Kumar <arun.kumar@tothenew.com>
2016-03-14 10:23:17 +0530

    final changes before presentation. made console clear

--- client/app/complain/complain.controller.js
+++ client/app/complain/complain.controller.js
@@ -4,7 +4,6 @@ angular.module('prwithyomanApp')
   .controller('ComplainCtrl',['$stateParams','$http', function ( $stateParams, $http) {
     var complainDetails = this;
     $http.get('/api/complainss/getComplain/'+$stateParams.complainId).then(function(data){
-      console.log(data.data);
       complainDetails.mainDetails = data.data.complain;
       complainDetails.timeline = data.data.timeline;
     });
